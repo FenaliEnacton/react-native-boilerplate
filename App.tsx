6b11faf597ed5e0f394cda31b19b11c754ff67d2
FenaliEnacton <fhkhajanchiwala@enacton.com>
2024-07-18 15:53:00 +0530

    feat: facebook login implemented in android

--- App.tsx
+++ App.tsx
@@ -1,57 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import type {PropsWithChildren} from 'react';
-import React, {useEffect} from 'react';
-import {
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import React, {useEffect} from 'react';
+import {Button, Platform, SafeAreaView, useColorScheme} from 'react-native';
+import {AccessToken, LoginManager} from 'react-native-fbsdk-next';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const GOOGLE_ID =
   '484357899033-0vvh7lem2lia40pv91s8r7b4dpji1tv3.apps.googleusercontent.com';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -65,7 +19,6 @@ function App(): React.JSX.Element {
       webClientId: GOOGLE_ID,
       offlineAccess: true,
     });
-
     return () => {};
   }, []);
 
@@ -74,30 +27,29 @@ function App(): React.JSX.Element {
     console.log('🚀 ~ googleSingIn ~ userInfo:', userInfo);
   }
 
+  async function facebookLogin() {
+    if (Platform.OS === 'android') {
+      // LoginManager.setLoginBehavior('web_only');
+    }
+
+    LoginManager.logInWithPermissions(['public_profile', 'email'])
+      .then(result => {
+        if (result.isCancelled) {
+        } else {
+          AccessToken.getCurrentAccessToken().then(data => {
+            console.log('🚀 ~ AccessToken.getCurrentAccessToken ~ data:', data);
+          });
+        }
+      })
+      .catch(e => console.log(e));
+  }
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <Button onPress={googleSingIn} title="Google SingIn" />
+      <Button onPress={facebookLogin} title="Facebook SingIn" />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
