0e1bbd3e05ba7d81fd9283bea9cc64f5a790e5c4
Prince <pdhirapara@enacton.com>
2024-07-18 16:48:43 +0530

    refact: oneSignal Test android done

---
+++ src/TOneSignal.js
@@ -0,0 +1,19 @@
+import {LogLevel, OneSignal} from 'react-native-onesignal';
+
+// Add OneSignal within your App's root component
+const App = () => {
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  // OneSignal Initialization
+  OneSignal.initialize('d12424f0-8bf9-458b-8d8b-da0235c02fbe');
+
+  // requestPermission will show the native iOS or Android notification permission prompt.
+  // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.Notifications.requestPermission(true);
+
+  // Method for listening for notification clicks
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
+};
