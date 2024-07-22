bfd6aff6421505ceb45de1a8d88fb3130afb0911
FenaliEnacton <fhkhajanchiwala@enacton.com>
2024-07-22 11:33:31 +0530

    fix: dynamic import issue fixed

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+        alias: {
+          '@assets': './src/Assets/',
+          '@components/core': './src/Components/Core/',
+          '@components/generic': './src/Components/Generic/',
+          '@components/user': './src/Components/User/',
+          '@react-native-config': './src/react-native-config/',
+          '@app_redux': './src/Redux/',
+          // '@app_redux/Actions': './src/Redux/Actions/',
+          '@user_redux': './src/Redux/user_redux/',
+          '@translations': './src/translations/',
+        },
+      },
+    ],
+  ],
 };
